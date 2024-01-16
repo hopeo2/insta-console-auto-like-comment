adad100a120f0d4ae07721c515a7b581aee5fe80
hopeo2 <omidobeidzadeh@gmail.com>
2024-01-16 23:46:38 +0330

    first

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+hello))
+
+Welcome to Delete Tweets & Likes Script 👋
+Twitter: user_nnnn
+
+if you want to delete all of your tweets and the tweets that you like, this script can help you.
+
+🟢🟢it's all free!🟢🟢
+How To Run This Script
+like & comment posts by specific hashtagg:https://www.instagram.com/accounts/login/ from your PC browser.
+2️⃣ go to your explore page: https://www.instagram.com/explore/tags/{zahedan}/ 
+3️⃣ right click and select inspect
+4️⃣ go to Console page
+5️⃣ copy code from index.js file and paste them is the Console page and press Enter button
---
+++ index.js
@@ -0,0 +1,125 @@
+var likeCount = 0;
+var commentCount = 0;
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function loop() {
+    console.log("Script loading…please wait…");
+    var Time1 = Math.ceil(Math.random() * 10) + 3;
+    var Time2 = Math.ceil(Math.random() * 10) + 3;
+    var randomSkip = Math.random() * 10;
+
+    if (likeCount % 5 === 0 && likeCount != 0) {
+        console.log("Reached likeCount of sqrt(25). Pausing for 2 minutes...");
+        await sleep(2 * 60 * 1000);
+    } else if(likeCount == 20) {
+        console.log("Reached likeCount of sqrt(25). Pausing for 6 minutes...");
+        await sleep(6 * 60 * 1000);
+    }
+
+    setTimeout(function () {
+        //like
+        if (document.querySelectorAll('svg[aria-label="Unlike"]')[0]) {
+            likeCount++;
+            console.log("Already Liked");
+        } else if (parseInt(randomSkip) >= 7) {
+            //we skip some of the post, we dont want to be a heart sleuth
+            likeCount++;
+            console.log("Random Skipping: " + parseInt(randomSkip));
+        } else {
+            likeCount++;
+            console.log("Liking the post");
+            //Like
+            var btn = document.querySelectorAll('svg[aria-label="Like"]');
+            btn[0].parentElement.click();
+        }
+
+        //comment
+        if (parseInt(randomSkip) >= 7) {
+            console.log("Random Skipping: " + parseInt(randomSkip));
+        } else if (
+            document.querySelectorAll('textarea[aria-label="Add a comment…"]')
+        ) {
+            var btn = document.querySelectorAll(
+                'textarea[aria-label="Add a comment…"]'
+            );
+            if (btn.length) {
+                btn[0].click();
+                console.log("commenting... the post");
+                btn[0].textContent =
+                    "چرا آدما دست از سر آدما بر نمیدارن؟؟! #توماج_صالحی";
+                var inputEvent = new Event("input", {
+                    bubbles: true,
+                    cancelable: true,
+                });
+                btn[0].dispatchEvent(inputEvent);
+                if (
+                    document.querySelectorAll(
+                        "body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe.x1qjc9v5.xjbqb8w.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xr1yuqi.xkrivgy.x4ii5y1.x1gryazu.x15h9jz8.x47corl.xh8yej3.xir0mxb.x1juhsu6 > div > article > div > div._ae65 > div > div > div._ae2s._ae3v._ae3w > section._aaoe._ae5y._ae5z._ae62 > div > form > div > div._am-5 > div"
+                    )
+                ) {
+                    var postBtn = document.querySelectorAll(
+                        "body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe.x1qjc9v5.xjbqb8w.x1lcm9me.x1yr5g0i.xrt01vj.x10y3i5r.xr1yuqi.xkrivgy.x4ii5y1.x1gryazu.x15h9jz8.x47corl.xh8yej3.xir0mxb.x1juhsu6 > div > article > div > div._ae65 > div > div > div._ae2s._ae3v._ae3w > section._aaoe._ae5y._ae5z._ae62 > div > form > div > div._am-5 > div"
+                    );
+                    if (postBtn[0].textContent == "Post") {
+                        postBtn[0].click();
+                        console.log("post comment");
+                    }
+                } else {
+                    console.log("post btn not found");
+                }
+            } else {
+                console.log("comment btn not found");
+            }
+
+            commentCount++;
+        }
+
+        //next
+        setTimeout(function () {
+            if (document.querySelectorAll("svg[aria-label='Next']").length) {
+                console.log("Loading next post");
+                console.log("Totally liked : " + likeCount);
+                console.log("Totally comment : " + commentCount);
+                var btn = document.querySelectorAll("svg[aria-label='Next']");
+                btn[0].parentElement.click();
+            } else {
+                console.log("No more posts on this tag!");
+            }
+            loop();
+        }, Time1 * 1000);
+    }, Time2 * 1000);
+}
+loop();
+
+
+
+
+
+
+
+
+
+
+var timeout = 5;
+
+var deletedTweets = 0;
+timeout = timeout * 1000
+
+var delTweets = function () {
+    console.log('liked:', deletedTweets);
+
+    document.querySelectorAll('[data-testid="like"]').forEach(function (v3, i3, a3) {
+        try {
+            v3.click();
+            deletedTweets++
+        } catch (error) {
+            console.error(error)
+        }
+    });
+
+    window.scrollBy(0, 10000);
+}
+setInterval(delTweets, timeout);
